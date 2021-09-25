3d1208df30d4542366f861e48ee6a44f130063e3
Shubhayu Shrestha <shresthashubhayu@gmail.com>
2021-09-25 01:20:47 -0500

    Update sxs4805_lab01.py

--- sxs4805_lab01.py
+++ sxs4805_lab01.py
@@ -5,55 +5,43 @@
 # Import Statements
 import os
 
+# recursive function that will search through the directory path through all of the folders until
+# a file is seen and will add that file size to the total size
 def directory_search(current_directory):
-    # DEBUG printing current directory
-    print("\n\t"+current_directory)
-
     # Storing the files and sub-directories from current directory in an array
     files = os.listdir(current_directory)
     
-    # DEBUG: printing contents of array
-    print("\n\nPRINTING ARRAY \n")
-    print(files)
-
+    # initializing variable that will hold the total file size
     total_size = 0
-    # looping through the files in the directory
+
+    # looping through the files in the current working directory and checking if it is either
+    # a file or a directory, if it is a directory, it will recursively call the function
+    # if it is a file, it will check the file size and add it to the total size
     for x in files:
-        # storing a variable that holds the next path 
+        # creating variable that holds the next path
         nextpath = os.path.join(current_directory, x)
 
+        # if the next file is a directory, then we will recursively call this function 
         if os.path.isdir(nextpath):
-            # DEBUG
-            print("\n\nCHANGING DIRECTORY TO " + x)
+            #if directory is found, change the directory 
+            total_size += directory_search(nextpath)
 
-            #if directory is found, change the directory
-            total_size+=directory_search(nextpath)
-            
+        # if the next file is a file, then we will check the size and add it onto total size
         elif os.path.isfile(nextpath):
-            file_size = 0
+            # initializing file_size - a temp variable that will hold the current size of the file
             file_size = os.path.getsize(nextpath)
-
-            # DEBUG
-            print(x + " is a file and is ", file_size, " bytes")
-
-            # checking and adding the size of the file to the TotalSize
+            # adding file size to total size
             total_size += file_size
-    
+
     return total_size
 
-
-# DEBUG
-print("\n\n\n\n\nSTARTING HERE")
 
 # Checking the current initial working directory and storing the current working directory
 # in a variable called path
 # User or GTA can also change this file path to another file path on their computer
 # path variable that holds the current working directory
 current_directory = os.getcwd()
-print("\n\nTOTAL SIZE: %d BYTES..." %directory_search(current_directory))
-        
 
-
-
-
-
+# print statement that will print the total number of bytes after searching
+# through the current directory
+print("TOTAL SIZE: %d bytes" % directory_search(current_directory))
